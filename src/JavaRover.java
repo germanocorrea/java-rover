813bc16f6a39c2cf2d8f1f49db37fd29d4e1b41e
Germano Corrêa <germanobruscato@gmail.com>
2018-06-25 20:41:19 -0300

    Changed main method

--- src/JavaRover.java
+++ src/JavaRover.java
@@ -1,14 +1,45 @@
 public class JavaRover {
 
     public static void main(String[] args) {
-        TerrainRobot robot = new TerrainRobot(new Position(39, 25, true), new Position(45, 20, true));
         Terrain surface = new Terrain(50);
-
         createTheLand(surface);
+
+        TerrainRobot[] robots = {
+                new TerrainRobot(new Position(30, 40, true), new Position(48, 48, true)),
+                new TerrainRobot(new Position(36, 6, true), new Position(36, 40, true)),
+                new TerrainRobot(new Position(10, 40, true), new Position(30, 22, true))
+        };
+
+        for (TerrainRobot robot: robots) {
+            makeRobotWalk(robot, surface);
+        }
+
+        System.out.println("ROBÔ TERRESTRE:\n");
+        surface.printTerrainRepresentation();
+        System.out.println("\n=================================================================\n");
+        System.out.println("ROBÔ BARCO:\n");
+
+        surface = new Terrain(50);
+        createTheLand(surface);
+
+        BoatRobot[] boatRobots = {
+                new BoatRobot(new Position(30, 40, true), new Position(48, 48, true)),
+                new BoatRobot(new Position(36, 6, true), new Position(36, 40, true)),
+                new BoatRobot(new Position(10, 40, true), new Position(30, 22, true))
+        };
+
+        for (BoatRobot robot: boatRobots ) {
+            makeRobotWalk(robot, surface);
+        }
+
+        surface.printTerrainRepresentation();
+    }
+
+    public static void makeRobotWalk(Robot robot, Terrain surface) {
         robot.terrain = surface;
 
-        surface.addEntity(robot.getPosition(), Position.ANSI_PURPLE + getRobotEmoji() + Position.ANSI_RESET);
-        surface.addEntity(robot.getFinalDestination(), Position.ANSI_RED + 'x' + Position.ANSI_RESET);
+        surface.addEntity(robot.getPosition(), Position.ANSI_PURPLE + " " + getRobotEmoji() + " " + Position.ANSI_RESET);
+        surface.addEntity(robot.getFinalDestination(), Position.ANSI_RED + " x " + Position.ANSI_RESET);
 
         boolean notDone = true;
         robot.defineCurrentTarget();
@@ -16,10 +47,9 @@ public class JavaRover {
             robot.stepAhead();
             if (robot.getPosition().equals(robot.getFinalDestination()))
                 notDone = false;
-            else surface.addEntity(robot.getPosition(), Position.ANSI_YELLOW + '+' + Position.ANSI_RESET);
+            else surface.addEntity(robot.getPosition(), Position.ANSI_YELLOW + " + " + Position.ANSI_RESET);
         }
 
-        surface.printTerrainRepresentation();
     }
 
     public static String getRobotEmoji() {
